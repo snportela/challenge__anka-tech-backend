8c938c8a420599f4c698509ff6c86cb300683764
Sophia Portela <sophianobreportela@gmail.com>
2025-06-06 00:07:54 -0300

    feat - cors configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import Fastify from "fastify";
 import clientRoutes from "./modules/clients/client.route";
 import { assetRoutes } from "./modules/assets/asset.route";
+import fastifyCors from "@fastify/cors";
 
 const app = Fastify();
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
 
 const PORT: number = parseInt(<string>process.env.PORT, 10) || 5000;
 
